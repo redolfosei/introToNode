010b502131998fd92fcce851498680507eea0db2
redolf <redolf639@gmail.com>
2022-07-30 22:13:22 +0000

    just a test in here

---
+++ index.js
@@ -0,0 +1,22 @@
+//var variable 
+//const constant - the value for const is not changeable; 
+//initialise npm, npm init; 
+/*  Internal nodeJS module;
+const pi = 3.14;
+const fs = require("fs"); //file system have been required and can now be used; 
+
+fs.copyFileSync("filetest.txt","filetest2.txt");
+*/
+
+var superheroes = require('superheroes');
+const supervillains = require('supervillains'); 
+
+var buddies = superheroes.random();
+supervillains.all;
+var hommies = supervillains.random(); 
+
+console.log("SuperHeroes: " + buddies);
+console.log("Supervillains: " + hommies); 
+
+
+
